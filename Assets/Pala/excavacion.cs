c3b86112529c67b8c9bca7695c89b11c94ca579f
Sergi <sergisise@gmail.com>
2022-12-27 14:22:41 +0100

    Escena excavacion creada con sonar y pala

--- Assets/Pala/excavacion.cs
+++ Assets/Pala/excavacion.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.Extras;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
 
 public class excavacion : MonoBehaviour
 {
     Animator animator;
     public GameObject objetoExcavacion;
     public GameObject objetoDesaparecido;
+    public GameObject particulas;
+    public SteamVR_Action_Boolean cogeObjeto;
     private void Start()
     {
         animator = gameObject.GetComponent<Animator>();
@@ -15,24 +21,28 @@ public class excavacion : MonoBehaviour
     {
         if (other.tag == "pala")
         {
-            Debug.Log("pala");
+            //Debug.Log("pala");
             objetoExcavacion.SetActive(true);
             animator.Play("objeto_saliendo");
+            particulas.SetActive(false);
         }
         
     }
 
     private void Update()
     {
-        // Debug.Log("hola");
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (cogeObjeto[this.GetComponent<Hand>().handType].stateDown)
         {
+           
             if (objetoExcavacion.activeSelf)
             {
                 objetoExcavacion.SetActive(false);
                 animator.gameObject.SetActive(false);
                 objetoDesaparecido.SetActive(true);
             }
+
+            //Debug.Log("Apretado");
         }
     }
 }
--- Assets/sonarEvent.cs
+++ Assets/sonarEvent.cs
@@ -8,19 +8,27 @@ using Valve.VR.InteractionSystem;
 public class sonarEvent : MonoBehaviour
 {
     public SteamVR_Action_Boolean activaSonar;
+    public GameObject anillo;
+    Animator animator_anillo;
 
     bool haSidoApretado = false;
+    private void Start()
+    {
+        animator_anillo = anillo.GetComponent<Animator>();
+    }
     private void Update()
     {
         if (activaSonar[this.GetComponent<Hand>().handType].stateDown)
         {
             haSidoApretado = true;
-            Debug.Log("Apretado");
+            animator_anillo.Play("abrirSonar");
+            //Debug.Log("Apretado");
         }
-        if (activaSonar[this.GetComponent<Hand>().handType].stateUp)
+        if (activaSonar[this.GetComponent<Hand>().handType].stateUp && haSidoApretado)
         {
-            Debug.Log("Soltado");
+            //Debug.Log("Soltado");
             haSidoApretado = false;
+            animator_anillo.Play("cerrarSonar");
         }
     }
 }
